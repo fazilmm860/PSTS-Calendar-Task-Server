a8c46a4adbce1925d6c5eeed3bec1748562ed38a
Mohamed Fazil M <fazilmm860@gmail.com>
2023-09-17 13:46:08 +0530

    edit in bookingSchema

--- models/bookingSchema.js
+++ models/bookingSchema.js
@@ -1,13 +1,30 @@
-// bookingSchema.js
 const mongoose = require('mongoose');
 
+
 const bookingSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  mobile: { type: String, required: true },
-  email: { type: String, required: true },
-  selectedSlot: { type: Date, required: true },
-  assignedRecruiter: { type: String, required: true },
-}, { timestamps: true });
+name: { 
+    type: String, 
+    required: true 
+},
+mobile: {
+     type: String, 
+     required: true
+},
+email: {
+     type: String,
+      required: true 
+},
+  selectedSlot: {
+     type: Date,
+      required: true 
+},
+  assignedRecruiter: {
+     type: String,
+      required: true 
+},
+}, 
+{ timestamps: true }
+);
 
 const Booking = mongoose.model('BookingSchema', bookingSchema);
 
